42028d57e532d4564ce08a01e5d3773bca6187c7
woodongle <315ehdgur@gmail.com>
2024-05-27 16:47:30 +0900

    test 패키지 생성

---
+++ src/test/vo/EventVo.java
@@ -0,0 +1,51 @@
+package test.vo;
+
+import java.util.Date;
+
+public class EventVo {
+    private int id;
+    private String event;
+    private Date startDate;
+    private Date endDate;
+    private int eventCalendarId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getEventCalendarId() {
+        return eventCalendarId;
+    }
+
+    public void setEventCalendarId(int eventCalendarId) {
+        this.eventCalendarId = eventCalendarId;
+    }
+}
