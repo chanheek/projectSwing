55c35f956230d6ec2f09361bc2c51795b956acc2
chanheek <toadfwe1@naver.com>
2024-05-29 11:26:44 +0900

    Merge branch 'master' of https://github.com/chanheek/projectSwing

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -88,7 +88,7 @@ public class MainFrame extends JFrame {
 
         JTextField majorField = new JTextField(10);
         if (userVo.getMajor() != null) {
-            majorField.setText(String.valueOf(userVo.getGrade()));
+            majorField.setText(String.valueOf(userVo.getMajor()));
         } else {
             majorField.setText("MANAGER");
         }
